7af19115e68c1aa649f57b5cd10c32f3847cc9a0
Aryan39232 <20bcs097@iiitdwd.ac.in>
2023-08-11 00:31:50 +0530

    done

--- server/router/auth.js
+++ server/router/auth.js
@@ -9,32 +9,9 @@ router.get('/' , (req , res) =>{
     res.send(`hello world from the server the server rotuer js`);
 });
 
-// router.post('/register' , (req , res) =>{
-
-//     const {name , email , phone , work , password , cpassword} = req.body;
-    
-//     if(!name || !email || !phone || !work || !password || !cpassword){
-//        return res.status(422).json({error:"You Need to field all required information"});
-//     }
-
-//     User.findOne({email: email})
-//         .then((userExist) => {
-//             if(userExist){
-//                 return res.status(422).json({error:"Email already Exits"});
-//             }
-//             const user = new User({name , email , phone , work , password , cpassword})
-
-//             user.save()
-//                 .then(() =>{
-//                     res.status(200).json({message : "user registered successfuly"})
-//                 })
-//                 .catch((error) =>{ res.status(500).json({error : "Fail to upload value"}) })
-//         })
-//         .catch((error) => {console.log(err);});
-// })
-
 router.post('/register' , async (req , res) =>{
     const { name , email , phone ,work , password , cpassword } = req.body;
+
 
     if(!name || !email || !phone || !work || !password || !cpassword){
         return res.status(422).json({error:"You Need to field all required information"});
@@ -67,8 +44,6 @@ router.post('/signin' , async (req , res) => {
         const userlogin = await User.findOne({email:email});
 
     }catch(error){  console.log("Invail credentials")  }
-    
-
 
 })
 
