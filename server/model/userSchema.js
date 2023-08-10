4d391a10f078ef10e41ddd1c7f665a4a13b97b4b
Aryan39232 <20bcs097@iiitdwd.ac.in>
2023-08-11 00:27:28 +0530

    Password hashing using Bcrypt

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+
+const bcrypt = require('bcryptjs')
 const userSchema = new mongoose.Schema({
     name:{
         type : String,
@@ -24,6 +26,15 @@ const userSchema = new mongoose.Schema({
         type : String,
         required : true
     }
+})
+
+
+userSchema.pre('save' ,async function(next){
+    if(this.isModified('password')){
+        this.password = bcrypt.hash(this.password , 12);
+        this.cpassword = bcrypt.hash(this.cpassword , 12);
+    }
+    next();
 })
 
 
