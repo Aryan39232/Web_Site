3049dc1db0f574fc30dc9050baa60ac0d83fe6dc
Aryan39232 <20bcs097@iiitdwd.ac.in>
2023-09-09 09:36:04 +0530

    Signup Form

---
+++ client/src/components/Signup.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <>
+        <section className="singup">
+          <div className="container mt-5">
+            <div className="signup-content">
+              <div className="signup-form">
+                <h2 className="form-title"> Sign Up </h2>
+                <form className="register-form" id="register-form">
+                    
+                </form>
+              </div>
+            </div> 
+          </div>
+        </section>
+    </>
+  )
+}
+
+export default Signup
